cee2946154ac3dcfe04eb77da274807d1adcd3e5
ConsoleRunner <wojciechly@wp.pl>
2020-11-28 20:25:36 +0100

    feat: iosco parser update

--- modules/IOSCO/iosco.py
+++ modules/IOSCO/iosco.py
@@ -14,17 +14,17 @@ class CompanyInfo:
         self.name = company_name
         self.regulator = regulator
         self.jurisdiction = jurisdiction
-        self.date = dt.datetime.strptime(date.split("\n")[-1], "%d %b %Y")
+        self.date = dt.datetime.strptime(date.replace('\t', '').split('\n')[-1], '%d %b %Y')
         self.subject = subject
-        if comments == "No additional comments posted.":
-            self.comments = "None"
+        if comments == 'No additional comments posted.':
+            self.comments = 'None'
         else:
             self.comments = comments
-            indx = self.comments.find("\tmore\n")
+            indx = self.comments.find('\tmore\n')
             if indx != -1:
                 self.comments = self.comments[:indx]
-            self.comments = self.comments.replace("\t", "").replace(
-                "\n", "").replace("\r", "")
+            self.comments = self.comments.replace('\t', '').replace(
+                '\n', '').replace('\r', '')
 
         self.redirect = None
 
@@ -34,14 +34,15 @@ class CompanyInfo:
 
 class IOSCOParse:
     def __init__(self) -> None:
-        self.hard_url = "https://www.iosco.org/investor_protection/?subsection=investor_alerts_portal"
+        self.hard_url = 'https://www.iosco.org/investor_protection/investor_al' \
+                        'erts/_investor_alerts.cfm?Rows=1000000&CurrentPage=0'
         self.hard_savepoint = 'iosco.html'
 
     def parse_loop(self) -> List[CompanyInfo]:
-        if os.path.isfile(self.hard_savepoint):
+        if False and os.path.isfile(self.hard_savepoint):
             with open(self.hard_savepoint, 'rb') as f:
                 html_code = f.read()
-            print("READ FROM FILE")
+            print('READ FROM FILE')
         else:
             with urllib.request.urlopen(self.hard_url) as url_handl:
                 # save the html code
@@ -50,18 +51,24 @@ class IOSCOParse:
                     with open(self.hard_savepoint, 'wb') as f:
                         f.write(html_code)
 
-        soup = BeautifulSoup(html_code, "html.parser")
-        table_body = soup.find("table", {"id": "tbl_inv_alerts_data"})
-        table_entries = table_body.find_all("tr")
+        soup = BeautifulSoup(html_code, 'html.parser')
+        table_body = soup.find_all('table')[0]
+        table_entries = table_body.find_all('tr')
 
         company_list: List[CompanyInfo] = []
         for entry in table_entries[1:]:
-            entries = entry.find_all("td")
+            entries = entry.find_all('td')
             lst = [x.text.strip() for x in entries]
             assert len(lst) == 6
 
             cmp = CompanyInfo(*lst)
-            redir = entries[3].find('a').attrs['href']
+            try:
+                redir = entries[3].find('a').attrs['href']
+            except Exception:
+                print('==========')
+                for a in lst:
+                    print(a)
+
             cmp.set_redirect(redir)
             company_list.append(cmp)
 
@@ -72,9 +79,9 @@ class IOSCOParse:
         return pd.DataFrame.from_records([c.__dict__ for c in comp_list])
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     p = IOSCOParse()
     cl = p.parse_loop()
     df = IOSCOParse.to_pandas(cl)
     df['index'] = df.index
-    df.to_csv("iosco.tsv", sep='\t', quotechar="\'", quoting=csv.QUOTE_NONE)
+    df.to_csv('iosco.tsv', sep='\t', quotechar='\'', escapechar='\\', quoting=csv.QUOTE_NONE)
