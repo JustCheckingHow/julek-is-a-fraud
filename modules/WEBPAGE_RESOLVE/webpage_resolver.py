af58f13b220935b2978f94bc465c6eae3a48fc95
Witold Wydmański <wwydmanski@gmail.com>
2020-11-28 04:49:18 +0100

    feat: add uk domain

--- modules/WEBPAGE_RESOLVE/webpage_resolver.py
+++ modules/WEBPAGE_RESOLVE/webpage_resolver.py
@@ -6,7 +6,7 @@ import os
 
 
 class WebpageResolver(DataSource):
-    DOMAINS = [".com", ".org", ".pl", ".eu",".net"]
+    DOMAINS = [".com", ".org", ".pl", ".eu",".net", ".co.uk"]
     CACHE_LOC = "modules/WEBPAGE_RESOLVE/cache.tsv"
     PAGE_CACHE_LOC = "modules/WEBPAGE_RESOLVE/cache/"
 
