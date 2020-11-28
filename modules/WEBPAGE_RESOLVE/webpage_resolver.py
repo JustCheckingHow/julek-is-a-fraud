f88f79dbd309157b563556b88b1dfb8776aae602
Witold Wydmański <wwydmanski@gmail.com>
2020-11-28 02:48:00 +0100

    style: var extraction

--- modules/WEBPAGE_RESOLVE/webpage_resolver.py
+++ modules/WEBPAGE_RESOLVE/webpage_resolver.py
@@ -8,6 +8,7 @@ import os
 class WebpageResolver(DataSource):
     DOMAINS = [".com", ".org", ".pl", ".eu"]
     CACHE_LOC = "modules/WEBPAGE_RESOLVE/cache.tsv"
+    PAGE_CACHE_LOC = "modules/WEBPAGE_RESOLVE/cache/"
 
     def __init__(self, company_name):
         super().__init__(company_name)
@@ -18,17 +19,16 @@ class WebpageResolver(DataSource):
     def get_html(webpage):
         page_name = webpage.replace("http://", "")
 
-        all_pages = glob.glob("modules/WEBPAGE_RESOLVE/cache/*")
+        all_pages = glob.glob(WebpageResolver.PAGE_CACHE_LOC+"*")
         if any(map(lambda x: page_name == os.path.split(x)[-1], all_pages)):
-            with open("modules/WEBPAGE_RESOLVE/cache/"+page_name, "r") as f:
+            with open(WebpageResolver.PAGE_CACHE_LOC+page_name, "r") as f:
                 return f.read()
 
         page = requests.get(webpage)
-        with open("modules/WEBPAGE_RESOLVE/cache/"+page_name, "w") as f:
+        with open(WebpageResolver.PAGE_CACHE_LOC+page_name, "w") as f:
             f.write(page.text)
         
         return page.text
-
 
     def return_data(self, **kwargs) -> dict:
         if self.company_name in self.cache.index:
