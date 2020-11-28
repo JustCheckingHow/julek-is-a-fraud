a8eb039a9c0fa92d505b4c154cb9847e1faae2c3
LemurPwned <jakub.mojsiejuk@gmail.com>
2020-11-28 23:21:37 +0100

    forex

---
+++ modules/FOREX/forex.py
@@ -0,0 +1,20 @@
+from data_source import DataSource
+import json
+
+STATIC_FILE = 'modules/FOREX/all_companies.json'
+
+DATA = json.load(open(STATIC_FILE, 'r'))
+
+
+class ForexReview(DataSource):
+    def __init__(self, company_name) -> None:
+        super().__init__(company_name)
+
+    def return_data(self, **kwargs) -> dict:
+        for el in DATA:
+            if self.company_name in el['org'] or self.company_name in el[
+                    'company']:
+                print(el)
+                return {'FOREX': el}
+
+        return {'FOREX': ''}
