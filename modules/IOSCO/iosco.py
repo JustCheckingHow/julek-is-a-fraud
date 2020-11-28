37f54dcadc1f361018ea1f3b5d5e223e3aac3854
LemurPwned <jakub.mojsiejuk@gmail.com>
2020-11-28 02:28:43 +0100

    adding IOSCO parser

---
+++ modules/IOSCO/iosco.py
@@ -0,0 +1,75 @@
+import csv
+import datetime as dt
+import os
+import urllib.request
+from typing import List
+
+import pandas as pd
+from bs4 import BeautifulSoup
+import re
+
+
+class CompanyInfo:
+    def __init__(self, company_name, regulator, jurisdiction, date, subject,
+                 comments) -> None:
+        self.name = company_name
+        self.regulator = regulator
+        self.jurisdiction = jurisdiction
+        self.date = dt.datetime.strptime(date.split("\n")[-1], "%d %b %Y")
+        self.subject = subject
+        if comments == "No additional comments posted.":
+            self.comments = "None"
+        else:
+            self.comments = comments
+            indx = self.comments.find("\tmore\n")
+            if indx != -1:
+                self.comments = self.comments[:indx]
+            self.comments = self.comments.replace("\t", "").replace("\n", "")
+            # self.comments = re.escape(self.comments)
+
+
+class IOSCOParse:
+    def __init__(self) -> None:
+        self.hard_url = "https://www.iosco.org/investor_protection/?subsection=investor_alerts_portal"
+        self.hard_savepoint = 'iosco.html'
+
+    def parse_loop(self) -> List[CompanyInfo]:
+        if os.path.isfile(self.hard_savepoint):
+            with open(self.hard_savepoint, 'rb') as f:
+                html_code = f.read()
+            print("READ FROM FILE")
+        else:
+            with urllib.request.urlopen(self.hard_url) as url_handl:
+                # save the html code
+                html_code = url_handl.read()
+                if not os.path.isfile(self.hard_savepoint):
+                    with open(self.hard_savepoint, 'wb') as f:
+                        f.write(html_code)
+
+        soup = BeautifulSoup(html_code, "html.parser")
+        table_body = soup.find("table", {"id": "tbl_inv_alerts_data"})
+        table_entries = table_body.findAll("tr")
+
+        company_list: List[CompanyInfo] = []
+        for entry in table_entries[1:]:
+            lst = [x.text.strip() for x in entry.findAll("td")]
+            assert len(lst) == 6
+            # if "\t" in lst[-1]:
+            #     print(lst[-1])
+            cmp = CompanyInfo(*lst)
+            company_list.append(cmp)
+            if cmp.comments != "None":
+                print("-------")
+                print(cmp.name, cmp.comments.startswith("more"), cmp.comments)
+
+        return company_list
+
+    @staticmethod
+    def toPandas(comp_list: List[CompanyInfo]) -> pd.DataFrame:
+        return pd.DataFrame.from_records([c.__dict__ for c in comp_list])
+
+
+if __name__ == "__main__":
+    p = IOSCOParse()
+    cl = p.parse_loop()
+    df = IOSCOParse.toPandas(cl).to_csv("iosco.tsv", sep='\t')
