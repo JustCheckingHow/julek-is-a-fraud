1bfc03b811024e52dc9eb399910309190ae25645
Witold Wydmański <wwydmanski@gmail.com>
2020-11-28 19:41:51 +0100

    feat: add endpoints

--- modules/WEBPAGE_RESOLVE/__main__.py
+++ modules/WEBPAGE_RESOLVE/__main__.py
@@ -1,6 +1,5 @@
 from .webpage_resolver import WebpageResolver
 
-test = WebpageResolver("chuj")
+test = WebpageResolver("Zurich ")
 res = test.return_data()
 print(res)
-# print(WebpageResolver.get_links_from_page(res['webpage'][0]))
